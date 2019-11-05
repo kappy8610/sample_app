2b0d7d2875e68ecc90d242db2d95a326e53d821f
Uchi8610 <b1900686@planet.kanazawa-it.ac.jp>
2019-11-05 19:43:20 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
+  # get 'static_page/home'
   get 'static_page/help'
-  root 'application#hello'
+  get 'static_page/about'
+  get 'static_page/contact'
+  root 'static_page#home'
 end
