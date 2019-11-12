7b0e4ba0ad4313c1038dca7a5d113c4ed867a5c0
Uchi8610 <b1900686@planet.kanazawa-it.ac.jp>
2019-11-12 19:56:56 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_page#home'
-  get '/help' => 'static_page#help'
-  get '/about' => 'static_page#about'
-  get '/contact' => 'static_page#contact'
-  get 'signup' => 'users#new'
+  get '/help', to:'static_page#help'
+  get '/about', to:'static_page#about'
+  get '/contact', to:'static_page#contact'
+  get 'signup', to:'users#new'
 end
