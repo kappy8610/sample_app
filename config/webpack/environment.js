215c98fee60540f2d1d4f70b7fd64a4c37113be3
Uchi8610 <b1900686@planet.kanazawa-it.ac.jp>
2019-12-24 18:29:10 +0900

    bootstrapができない~その2~

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+const webpack = require('webpack')
 
-
-const webpack = require('webpack');
-environment.plugins.append(
-  'Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default'],
-  }),
-);
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
+module.exports = environment
